b182bcd49c20dd5c9819b74408daeb556092c27f
Toannx <mctoannguyen@gmail.com>
2018-06-07 16:55:41 +0700

    restore update league

--- app/src/main/java/com/football/interactors/service/IApiService.java
+++ app/src/main/java/com/football/interactors/service/IApiService.java
@@ -22,7 +22,6 @@ import okhttp3.RequestBody;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
-import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
@@ -102,7 +101,7 @@ public interface IApiService {
     @POST(ServiceConfig.CREATE_LEAGUE)
     Observable<BaseResponse<LeagueResponse>> createLeague(@Body RequestBody body);
 
-    @PUT(ServiceConfig.UPDATE_LEAGUE)
+    @POST(ServiceConfig.UPDATE_LEAGUE)
     Observable<BaseResponse<LeagueResponse>> updateLeague(@Path(ServiceConfig.KEY_LEAGUE_ID) int leagueId, @Body RequestBody body);
 
     @POST(ServiceConfig.CREATE_TEAM)
