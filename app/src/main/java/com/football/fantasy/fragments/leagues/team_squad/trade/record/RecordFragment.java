f6549113e9cb5dcc0d41206dae54cf575a914953
Toannx <mctoannguyen@gmail.com>
2018-07-03 09:54:32 +0700

    record item

--- app/src/main/java/com/football/fantasy/fragments/leagues/team_squad/trade/record/RecordFragment.java
+++ app/src/main/java/com/football/fantasy/fragments/leagues/team_squad/trade/record/RecordFragment.java
@@ -12,6 +12,7 @@ import com.football.fantasy.R;
 import com.football.models.responses.RecordResponse;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 
@@ -46,7 +47,12 @@ public class RecordFragment extends BaseMainMvpFragment<IRecordView, IRecordPres
     }
 
     void initRecyclerView() {
-        RecordAdapter adapter = new RecordAdapter(new ArrayList<>());
+        List<RecordResponse> records = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            records.add(new RecordResponse());
+        }
+
+        RecordAdapter adapter = new RecordAdapter(records);
         rvRecord.adapter(adapter)
                 .refreshListener(() -> {
                     refreshData();
