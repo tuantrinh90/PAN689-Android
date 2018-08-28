3414e8fc057ae959b72ddf11d884cdeaa781f814
Toannx <mctoannguyen@gmail.com>
2018-08-28 16:17:16 +0700

    notification keys

---
+++ app/src/main/java/com/football/services/NotificationKey.java
@@ -0,0 +1,36 @@
+package com.football.services;
+
+public class NotificationKey {
+    public static final String user_leaved_league = "user_leaved_league";
+    public static final String user_joined_league = "user_joined_league";
+    public static final String user_accept_invite = "";
+    public static final String user_reject_invite = "";
+    public static final String user_receive_invite = "";
+    public static final String time_setup_time = "";
+    public static final String full_team = "";
+    public static final String before_team_setup_time_1h = "";
+    public static final String before_team_setup_time_2h = "";
+    public static final String cancel_league_since_lack_member = "";
+    public static final String cancel_league_since_owner = "";
+    public static final String random_team = "";
+    public static final String complete_setup_team = "";
+    public static final String before_start_time_2h = "";
+    public static final String start_league = "";
+    public static final String edit_league = "";
+    public static final String owner_delete_member = "";
+    public static final String change_owner_league = "";
+    public static final String change_team_name = "";
+    public static final String change_league_name = "";
+    public static final String player_injured = "";
+    public static final String player_new_join = "";
+    public static final String player_has_left = "";
+    public static final String before_transfer_deadline_2h = "";
+    public static final String newest_real_result = "";
+    public static final String newest_game_result = "";
+    public static final String value_of_player_changed = "";
+    public static final String time_real_match_changed = "";
+    public static final String score_of_real_match_has_been_adjusted = "";
+    public static final String league_finish = "";
+    public static final String league_finish_for_champion = "";
+
+}
